c2d93126e8b94e859cbef5ef0e0afbef69a211e3
Zenqirtz <argad5033@gmail.com>
2026-09-08 15:18:49 +0700

    fix(js): add defaultContent for datatables action column in estimasi-disetujui

--- public/assets/js/estimasi-disetujui.js
+++ public/assets/js/estimasi-disetujui.js
@@ -92,7 +92,7 @@
           { data: 'status_spk' },
           { data: 'no_polis' },
           { data: 'kode_claim' },
-          { data: 'action' }
+          { data: 'action', defaultContent: '' }
        ],
        columnDefs: [
         //  {
