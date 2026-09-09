02cc3318a3e6221a6b9c1afe15685fb61be9911f
Zenqirtz <argad5033@gmail.com>
2026-09-09 12:19:29 +0700

    fix(js): add defaultContent for datatables detail action column in order-pembelian

--- public/assets/js/order-pembelian.js
+++ public/assets/js/order-pembelian.js
@@ -298,7 +298,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
           { data: 'qty' },
           { data: 'harga' },
           { data: 'jumlah' },
-          { data: 'action' }
+          { data: 'action', defaultContent: '' }
         ],
         columnDefs: [
           {
