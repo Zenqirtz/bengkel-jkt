9e7a5f5c22dfff7b79152721ae230d04fa69c5b8
Zenqirtz <argad5033@gmail.com>
2026-09-17 11:44:21 +0700

    perf(customer-service): remove redundant count query in LaporanOutstandingController periode report

--- app/Http/Controllers/customer_service/LaporanOutstandingController.php
+++ app/Http/Controllers/customer_service/LaporanOutstandingController.php
@@ -105,7 +105,7 @@ class LaporanOutstandingController extends Controller
         $totalData = (clone $query)->count('k.id');
 
         // Hitung setelah filter (tanpa limit/offset)
-        $totalFiltered = (clone $query)->count('k.id');
+        $totalFiltered = $totalData;
 
         // Ambil data halaman saat ini
         $datas = $query
