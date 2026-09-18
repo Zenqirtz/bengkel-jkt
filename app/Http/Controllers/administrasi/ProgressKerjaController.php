6ab26178ea43f8d320e1b4d95232da47117949ce
Zenqirtz <argad5033@gmail.com>
2026-09-18 16:34:46 +0700

    fix(administrasi): implement destroy with JsonResponse and LogActivity in ProgressKerjaController

--- app/Http/Controllers/administrasi/ProgressKerjaController.php
+++ app/Http/Controllers/administrasi/ProgressKerjaController.php
@@ -677,9 +677,21 @@ class ProgressKerjaController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function destroy($id)
+  public function destroy($id): JsonResponse
   {
-    //$datas = Spk::where('id', $id)->delete();
+    $data = Spk::find($id);
+
+    if (!$data) {
+      return response()->json(['status' => false, 'message' => 'Data tidak ditemukan'], 404);
+    }
+
+    $dataArr = $data->toArray();
+    $ok = $data->delete();
+
+    $desc = $ok ? 'Berhasil Hapus Progress Kerja' : 'Gagal Hapus Progress Kerja';
+    LogActivity::saveLogActivity($desc, $dataArr);
+
+    return response()->json(['status' => (bool) $ok, 'message' => $desc]);
   }
 
 }
