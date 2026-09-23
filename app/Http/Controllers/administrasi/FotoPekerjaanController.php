9d0b19bd07976eedd5d6a459976e54a8237ed201
Zenqirtz <argad5033@gmail.com>
2026-09-23 15:37:27 +0700

    fix(administrasi): standardize update to return JsonResponse 501 in FotoPekerjaanController

--- app/Http/Controllers/administrasi/FotoPekerjaanController.php
+++ app/Http/Controllers/administrasi/FotoPekerjaanController.php
@@ -412,7 +412,7 @@ class FotoPekerjaanController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) {}
+  public function update(Request $request, $id): JsonResponse { return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501); }
 
   /**
    * Remove the specified resource from storage.
