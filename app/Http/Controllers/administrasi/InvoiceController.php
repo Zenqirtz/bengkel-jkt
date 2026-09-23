2853fb9d9f7c19d45a3caca3dcd78fa0c5509865
Zenqirtz <argad5033@gmail.com>
2026-09-23 15:39:35 +0700

    fix(administrasi): standardize update to return JsonResponse 501 in InvoiceController

--- app/Http/Controllers/administrasi/InvoiceController.php
+++ app/Http/Controllers/administrasi/InvoiceController.php
@@ -715,7 +715,7 @@ class InvoiceController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) {}
+  public function update(Request $request, $id): JsonResponse { return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501); }
 
   /**
    * Remove the specified resource from storage.
