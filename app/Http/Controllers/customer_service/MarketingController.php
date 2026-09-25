0e072f0f3efe99f0d5f34090dfb14b678e7caa07
Zenqirtz <argad5033@gmail.com>
2026-09-25 15:11:02 +0700

    fix(customer_service): standardize update to return JsonResponse 501 in MarketingController

--- app/Http/Controllers/customer_service/MarketingController.php
+++ app/Http/Controllers/customer_service/MarketingController.php
@@ -324,7 +324,10 @@ class MarketingController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) {}
+  public function update(Request $request, $id): JsonResponse
+  {
+    return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501);
+  }
 
   /**
    * Remove the specified resource from storage.
