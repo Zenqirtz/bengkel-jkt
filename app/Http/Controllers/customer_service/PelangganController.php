57ab2fdb735cca3d4408d31f6b85722a53b63acf
Zenqirtz <argad5033@gmail.com>
2026-09-25 15:27:03 +0700

    refactor(customer_service): block unimplemented update on PelangganController with 501 response

--- app/Http/Controllers/customer_service/PelangganController.php
+++ app/Http/Controllers/customer_service/PelangganController.php
@@ -420,9 +420,9 @@ class PelangganController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) 
+  public function update(Request $request, $id): JsonResponse
   {
-    //    
+    return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501);
   }
 
   /**
