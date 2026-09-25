7eb8ce53b9b8442c3f592cb27491c6b911f44e4a
Zenqirtz <argad5033@gmail.com>
2026-09-25 15:27:17 +0700

    style(customer_service): enforce consistent not-implemented response on SpkTutupController update

--- app/Http/Controllers/customer_service/SpkTutupController.php
+++ app/Http/Controllers/customer_service/SpkTutupController.php
@@ -415,7 +415,10 @@ class SpkTutupController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) {}
+  public function update(Request $request, $id): JsonResponse
+  {
+    return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501);
+  }
 
   /**
    * Remove the specified resource from storage.
