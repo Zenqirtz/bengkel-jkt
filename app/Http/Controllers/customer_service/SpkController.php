7bbee4ae9e1d592727a9c760325fda309617ab5b
Zenqirtz <argad5033@gmail.com>
2026-09-25 15:12:21 +0700

    fix(customer_service): standardize update to return JsonResponse 501 in SpkController

--- app/Http/Controllers/customer_service/SpkController.php
+++ app/Http/Controllers/customer_service/SpkController.php
@@ -506,7 +506,10 @@ class SpkController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id) {}
+  public function update(Request $request, $id): JsonResponse
+  {
+    return response()->json(['status' => false, 'message' => 'Fitur belum tersedia'], 501);
+  }
 
   /**
    * Remove the specified resource from storage.
